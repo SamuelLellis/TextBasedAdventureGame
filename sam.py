52f3f6054ec56e4999e1df9de23067ec5d436440
Samuel Lellis <slellis1@binghamton.edu>
2018-10-02 18:43:09 -0400

    Second Draft

--- sam.py
+++ sam.py
@@ -1,4 +1,14 @@
 def main():
+    print("You shield your eyes as they are suddenly assaulted by the sunlight upon the opening of the door")
+    print('"What on Earth?"')
+    print("You say to yourself as you take in the surrondings around you.")
+    print("You look to your right and observe an abandoned car.")
+    input1 = input("Would you like to investigate it? Yes or No?")
+    print()
+    if (input1.lower() == "yes"):
+        print("Deciding to give it a chance you approach the car with caution with your fists up in self defense,\n with the windows being blown out and all you don't want to take any chances.")
+    if (input1.lower() == "no"):
+        print("With a shake of your head you decide that the abandoned car can't do much for you, so you turn your head in the opposite direction to see if there is anything else outside of this desolate house that you were trapped in.")
 
 
 
